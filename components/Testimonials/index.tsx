a7edb31b9c02cee4910c44a24cd7983a82151b89
marcSitze <marcsitze01@gmail.com>
2022-11-22 19:52:33 +0100

    testimonial styles

--- components/Testimonials/index.tsx
+++ components/Testimonials/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from './Styles.module.css'
+import rStyles from './Responsive.module.css'
 import Testimonial from './Testimonial'
 
 const Testimonials = () => {
   return (
-    <div className='bg-smoke p-4 md:p-16 containerX flex overflow-x-scroll'>
+    <div className={`bg-smoke p-4 md:p-16 w-5/6 ${styles.testimonials} ${rStyles.testimonials} flex overflow-x-scroll`}>
       <Testimonial />
       <Testimonial />
       <Testimonial />
