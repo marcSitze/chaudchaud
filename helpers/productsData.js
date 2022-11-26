26eab62106afb3901827b8ebfc4487bad05df992
marcSitze <marcsitze01@gmail.com>
2022-11-27 00:39:52 +0100

    helper files

---
+++ helpers/productsData.js
@@ -0,0 +1,125 @@
+import img1 from '../assets/shirts/1.jpeg'
+import img2 from '../assets/shirts/2.jpeg'
+import img3 from '../assets/shirts/3.jpeg'
+import img4 from '../assets/shirts/4.jpeg'
+import img5 from '../assets/shirts/5.jpeg'
+import img6 from '../assets/shirts/6.jpeg'
+import img7 from '../assets/shirts/7.jpeg'
+import img8 from '../assets/shirts/8.jpeg'
+import img9 from '../assets/shirts/9.jpeg'
+import img10 from '../assets/shirts/10.jpeg'
+
+const products = [
+  {
+    id: 1,
+    name: "Comstock Holding Companies, Inc.",
+    description: "Nondisp fx of med phalanx of fngr, subs for fx w delay heal",
+    price: "2500",
+    size: "L",
+    color: ["#47aec5","#f6bbb6"],
+    stock: "08-102-9871",
+    gender: "M",
+    image: img1
+  },
+  {
+    id: 2,
+    name: "JetBlue Airways Corporation",
+    description: "Other secondary gout, unspecified wrist",
+    price: "2000",
+    size: "L",
+    color: ["#f6bbb6", "#9a23c9"],
+    stock: "49-337-9164",
+    gender: "F",
+    image: img2
+  },
+  {
+    id: 3,
+    name: "Berkshire Hathaway Inc.",
+    description: "Valgus deformity, not elsewhere classified, right knee",
+    price: "5000",
+    size: "S",
+    color: ["#9a23c9"],
+    stock: "50-523-2669",
+    gender: "M",
+    image: img3
+  },
+  {
+    id: 4,
+    name: "Bonso Electronics International, Inc.",
+    description: "Asphyxiation due to plastic bag",
+    price: "6500",
+    size: "XS",
+    color: ["#9c027d"],
+    stock: "82-188-0148",
+    gender: "F",
+    image: img4
+  },
+  {
+    id: 5,
+    name: "Post Holdings, Inc.",
+    description: "Disp fx of head of unsp rad, subs for clos fx w routn heal",
+    price: "2000",
+    size: "XS",
+    color: ["#c5697f"],
+    stock: "80-473-1680",
+    gender: "M",
+    image: img5
+  },
+  {
+    id: 6,
+    name: "VistaGen Therapeutics, Inc.",
+    description: "Sltr-haris Type IV physl fx upr end humer, r arm, 7thK",
+    price: "4000",
+    size: "3XL",
+    color: ["#f9d740", "#000", "fff", "00ff00"],
+    stock: "94-844-6390",
+    gender: "M",
+    image: img6
+  },
+  {
+    id: 7,
+    name: "Quantenna Communications, Inc.",
+    description: "Poisoning by other antiprotozoal drugs, assault, subs encntr",
+    price: "3000",
+    size: "M",
+    color: ["#b219ce"],
+    stock: "34-611-3239",
+    gender: "F",
+    image: img7
+  },
+  {
+    id: 8,
+    name: "Luxottica Group, S.p.A.",
+    description: "Displ commnt fx l patella, 7thF",
+    price: "4500",
+    size: "M",
+    color: ["#8fbd69"],
+    stock: "30-730-4183",
+    gender: "M",
+    image: img8
+  },
+  {
+    id: 9,
+    name: "PowerShares Water Resources Portfolio",
+    description: "Congenital malformations of esophagus",
+    price: "500",
+    size: "L",
+    color: ["#082492"],
+    stock: "87-748-2037",
+    gender: "F",
+    image: img9
+  },
+  {
+    id: 10,
+    name: "Dynegy Inc.",
+    description: "Traumatic rupture of unspecified ear drum, initial encounter",
+    price: "1500",
+    size: "XL",
+    color: ["#cf9a9d"],
+    stock: "50-415-9324",
+    gender: "F",
+    image: img10
+  },
+];
+
+export default products;
