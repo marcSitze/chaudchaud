6aafb561f6a282d31b09fee7133f59cf33e75556
marcSitze <marcsitze01@gmail.com>
2022-11-27 00:43:47 +0100

    state provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import BaseWrapper from "../components/Layouts/BaseWrapper";
+import { ProductsContextProvider } from "../context/products/productsContext";
+import { CartContextProvider } from "../context/cart/cartContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ProductsContextProvider>
+      <CartContextProvider>
+        <BaseWrapper>
+          <Component {...pageProps} />
+        </BaseWrapper>
+      </CartContextProvider>
+    </ProductsContextProvider>
+  );
 }
