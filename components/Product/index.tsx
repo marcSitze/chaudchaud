ea1b7dcf09a47ebde3c5c0915f2c9c7794f3495e
marcSitze <marcsitze01@gmail.com>
2022-11-27 00:42:01 +0100

    product component

--- components/Product/index.tsx
+++ components/Product/index.tsx
@@ -1,35 +1,56 @@
-import React from "react";
+import React, { useContext } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
 import Tshirt from "../../assets/tshirt.jpeg";
 import { faShippingFast, faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Product } from '../../types/products'
+import { cartContext } from "../../context/cart/cartContext";
 
-const Product = () => {
+const Product = ({ product }: { product: Product }) => {
+  const router = useRouter();
+  const { addToCart } = useContext(cartContext);
+
   return (
-    <div className={`w-full md:w-1/2 lg:w-1/3 h-80 flex flex-col items-center mb-10 md:pr-2`}>
+    <div
+      className={`w-full md:w-1/2 lg:w-1/3 h-80 flex flex-col mb-14 md:px-4`}
+    >
       <div className="overflow-hidden flex flex-col justify-center mb-4">
-        <Image src={Tshirt} alt="tshirt" />
+        <Image src={product?.image ?? Tshirt} alt="tshirt" />
       </div>
       {/* <div className={`h-full`}>
             </div> */}
-      <h4 className={`text-lg font-bold mb-2`}>Kaos Ngabers</h4>
-      <h5 className={`text-lg mb-2`}>$59</h5>
-      <div className={`flex`}>
-        <FontAwesomeIcon
-          style={{ width: 20, marginRight: 5 }}
-          color={"#f8bc4e"}
-          icon={faStar}
-        />
-        <FontAwesomeIcon
-          style={{ width: 20, marginRight: 5 }}
-          color={"#f8bc4e"}
-          icon={faStar}
-        />
-        <FontAwesomeIcon
-          style={{ width: 20, marginRight: 5 }}
-          color={"#f8bc4e"}
-          icon={faStar}
-        />
+      <div className="flex justify-between items-center">
+        <div>
+          {/* <h4 onClick={() => router.push(`/shop/${product.id}`)} className={`text-lg font-bold mb-2`}>Kaos Ngabers</h4> */}
+          <h4 className={`text-lg font-bold mb-2`}>
+            <Link href={`/shop/${product?.id}`}>{product?.name ?? "Kaos Ngabers"}</Link>
+          </h4>
+          <h5 className={`text-lg mb-2`}>{`${product?.price ?? '$59'} XAF`}</h5>
+          <div className={`flex`}>
+            <FontAwesomeIcon
+              style={{ width: 20, marginRight: 5 }}
+              color={"#f8bc4e"}
+              icon={faStar}
+            />
+            <FontAwesomeIcon
+              style={{ width: 20, marginRight: 5 }}
+              color={"#f8bc4e"}
+              icon={faStar}
+            />
+            <FontAwesomeIcon
+              style={{ width: 20, marginRight: 5 }}
+              color={"#f8bc4e"}
+              icon={faStar}
+            />
+          </div>
+        </div>
+        <div className="self-end">
+          <button onClick={() => addToCart(product)} className="bg-primary text-white font-bold text-lg rounded-full py-2 px-4">
+            Add to cart
+          </button>
+        </div>
       </div>
     </div>
   );
