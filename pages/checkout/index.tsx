b5ac6b019202c2a48e41d8d0b58a4f8a170602ad
marcSitze <marcsitze01@gmail.com>
2022-11-27 16:32:29 +0100

    reponsive styles

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -2,40 +2,42 @@ import React from "react";
 import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import styles from './Styles.module.css'
+import rStyles from './Responsive.module.css'
 
 const Checkout = () => {
   const router = useRouter()
   return (
-    <div>
-      <h2 className="px-8 mb-4 text-xl font-bold">Shipping informations</h2>
-      <form className="px-8 mb-8">
+    <div className={`${styles.container} ${rStyles.container}`}>
+      <h2 className="px-2 mb-4 text-xl font-bold">Shipping informations</h2>
+      <form className="px-2 mb-8">
         <div className="flex my-2">
-          <label className="text-sm w-1/3">First Name: </label>
-          <input className="flex-1 border-b" type="text" />
+          <label className="text-sm w-1/3 sm:text-base">First Name: </label>
+          <input className="flex-1 border-b sm:text-base" type="text" />
         </div>
         <div className="flex my-2">
-          <label className="text-sm w-1/3">Last Name: </label>
-          <input className="flex-1 border-b" type="text" />
+          <label className="text-sm w-1/3 sm:text-base">Last Name: </label>
+          <input className="flex-1 border-b sm:text-base" type="text" />
         </div>
         <div className="flex my-2">
-          <label className="text-sm w-1/3">Phone Number: </label>
-          <input className="flex-1 border-b" type="text" />
+          <label className="text-sm w-1/3 sm:text-base">Phone: </label>
+          <input className="flex-1 border-b sm:text-base" type="text" />
         </div>
         <div className="flex my-2">
-          <label className="text-sm w-1/3">Email Address: </label>
-          <input className="flex-1 border-b" type="text" />
+          <label className="text-sm w-1/3 sm:text-base">Email: </label>
+          <input className="flex-1 border-b sm:text-base" type="text" />
         </div>
         <div className="flex my-2">
-          <label className="text-sm w-1/3">Address: </label>
-          <input className="flex-1 border-b" type="text" />
+          <label className="text-sm w-1/3 sm:text-base">Address: </label>
+          <input className="flex-1 border-b sm:text-base" type="text" />
+        </div>
+        <div className="flex my-2">
+          <label className="text-sm w-1/3 sm:text-base">Point of contact: </label>
+          <input className="flex-1 border-b sm:text-base" type="text" />
         </div>
         <div className="flex mt-2 mb-8">
-          <label className="text-sm w-1/3">Point de repere: </label>
-          <input className="flex-1 border-b" type="text" />
-        </div>
-        <div className="flex mt-2 mb-8">
-          <label className="text-sm w-1/3">City: </label>
-          <input className="flex-1 border-b" type="text" />
+          <label className="text-sm w-1/3 sm:text-base">City: </label>
+          <input className="flex-1 border-b sm:text-base" type="text" />
         </div>
         <div className="flex justify-between">
           <p className="text-sm">Save for future purchases?</p>
