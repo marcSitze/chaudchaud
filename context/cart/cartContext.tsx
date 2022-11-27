e2134bf230cdd4d858ac1267bf74ca01f2c3bd09
marcSitze <marcsitze01@gmail.com>
2022-11-27 16:33:22 +0100

    add toast on actions

--- context/cart/cartContext.tsx
+++ context/cart/cartContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useReducer } from "react";
+import { useToasts } from 'react-toast-notifications'
 import { Product } from "../../types/products";
 import CartReducer from "./cartReducer";
 import * as types from './types'
@@ -12,11 +13,13 @@ export const cartContext = createContext(INITIAL_STATE)
 
 export const CartContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [state, dispatch] = useReducer(CartReducer, INITIAL_STATE)
+  const { addToast } = useToasts()
 
   function addToCart(item: Product) {
     const find = state.cart.find((i: ICartItem) => i.product.id === item.id)
     if(find) {
       console.log('find: ', find)
+      addToast('quantity updated', { appearance: 'success', autoDismiss: true, autoDismissTimeout: 2000})
       return dispatch({
         type: types.UPDATE_QUANTITY,
         payload: { id: find.product.id, quantity: find.quantity + 1 }
@@ -26,6 +29,7 @@ export const CartContextProvider = ({ children }: { children: React.ReactNode })
       type: types.ADD_TO_CART,
       payload: item
     })
+    addToast('item added successfully to cart', { appearance: 'success', autoDismiss: true, autoDismissTimeout: 2000})
   }
 
   function deleteCartItem(id: string) {
@@ -33,7 +37,7 @@ export const CartContextProvider = ({ children }: { children: React.ReactNode })
       type: types.DELETE_CART_ITEM,
       payload: id
     })
-
+    addToast('item removed from cart', { appearance: 'error', autoDismiss: true, autoDismissTimeout: 2000})
   }
   function updateQuantity(id: string, quantity: number) {
 
