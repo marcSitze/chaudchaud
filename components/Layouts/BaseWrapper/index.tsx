e54aaeabdcf5ac9497b2b6e9606c01dac1d32826
marcSitze <marcsitze01@gmail.com>
2022-11-27 13:37:27 +0100

    add classname

--- components/Layouts/BaseWrapper/index.tsx
+++ components/Layouts/BaseWrapper/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import styles from './Styles.module.css'
 
 const BaseWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Navbar />
-      <div style={{ paddingTop: 150, paddingBottom: 100 }}>{children}</div>
+      <div className={styles.container}>{children}</div>
       <Footer />
     </div>
   );
