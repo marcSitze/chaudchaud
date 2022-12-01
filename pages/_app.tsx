14caa50132611f6ade5e6ebece89a6807b313f73
marcSitze <marcsitze01@gmail.com>
2022-12-01 11:26:08 +0100

    order and payment context providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,25 @@
+import '@progress/kendo-theme-default/dist/all.css';
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastProvider } from "react-toast-notifications";
 import BaseWrapper from "../components/Layouts/BaseWrapper";
 import { ProductsContextProvider } from "../context/products/productsContext";
 import { CartContextProvider } from "../context/cart/cartContext";
+import { OrderContextProvider } from "../context/orders/orderContext";
+import { PaymentsContextProvider } from "../context/payments/paymentsContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ToastProvider>
       <ProductsContextProvider>
         <CartContextProvider>
-          <BaseWrapper>
-            <Component {...pageProps} />
-          </BaseWrapper>
+          <OrderContextProvider>
+            <PaymentsContextProvider>
+              <BaseWrapper>
+                <Component {...pageProps} />
+              </BaseWrapper>
+            </PaymentsContextProvider>
+          </OrderContextProvider>
         </CartContextProvider>
       </ProductsContextProvider>
     </ToastProvider>
