751ed0956d66e274a2dafd29545f09d6c1d5e906
marcSitze <marcsitze01@gmail.com>
2022-12-01 11:27:34 +0100

    add spinner

--- pages/thankyou/index.tsx
+++ pages/thankyou/index.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect} from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { orderContext } from "../../context/orders/orderContext";
+import { paymentsContext } from "../../context/payments/paymentsContext";
+import Spinner from '../../assets/spinner.gif'
 
 const ThankYou = () => {
+  const { creating } = useContext(orderContext)
+  const { paying } = useContext(paymentsContext)
+  useEffect(() => {
+
+    console.log('creating: ', creating)
+    console.log('paying: ', paying)
+  }, [creating, paying])
   return (
     <div className="text-center ">
-      <h2 className="font-bold text-lg mb-2">Thank You for ordering at chaud chaud</h2>
-      <Link href={'/shop'}>Continue Shopping</Link>
+      {(creating || paying) && <div className="flex justify-center">
+        <Image src={Spinner} alt="spinner"/>
+      </div>}
+      {paying && <h2 className="font-bold text-lg mb-2">Processing your payment...</h2>}
+      {(!creating && !paying) && <h2 className="font-bold text-lg mb-2">Thank You for ordering at chaud chaud</h2>}
+      <Link className="text-base text-secondary" href={'/shop'}>Continue Shopping</Link>
     </div>
   );
 };
