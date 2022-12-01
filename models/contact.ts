65183a68fcf133306aa20d59ed9ea825a7e83a8f
marcSitze <marcsitze01@gmail.com>
2022-12-01 11:24:22 +0100

    models

---
+++ models/contact.ts
@@ -0,0 +1,11 @@
+import { IContact } from "../types/contact"
+
+export const Contact: IContact = {
+  address: '',
+  city: '',
+  email: '',
+  firstName: '',
+  lastName: '',
+  phone: '',
+  point: ''
+}
